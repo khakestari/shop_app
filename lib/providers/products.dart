e4c8899b3fbff05f5add4ca03dd1ce7fd3858913
Mostafa Nematollah zade <mostafanmz@yahoo.com>
2023-06-22 23:11:08 +0330

    listen in some places & ways

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -46,6 +46,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
   void addProduct() {
     // _items.add(value);
     notifyListeners();
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -10,11 +13,11 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // extract arguments
     final productId = ModalRoute.of(context)!.settings.arguments as String;
-    // ...
-
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
