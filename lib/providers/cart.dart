e53055d4b34d0d70e7f52ca32bb93a516161183d
Mostafa Nematollah zade <mostafanmz@yahoo.com>
2023-07-04 18:15:32 +0330

    remove cart items by dismissible widget

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -56,4 +56,8 @@ class Cart with ChangeNotifier {
     }
     notifyListeners();
   }
+  void removeItem(String productId) {
+    _items.remove(productId);
+    notifyListeners();
+  }
 }
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   CartItem(
       {required this.id,
+      required this.productId,
       required this.price,
       required this.quantity,
       required this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsetsDirectional.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: FittedBox(child: Text('\$$price')),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 30,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: FittedBox(child: Text('\$$price')),
+              ),
             ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
