d8bb88197fedbbb02992662b1b173c1fa3bccc47
Mostafa Nematollah zade <mostafanmz@yahoo.com>
2023-07-05 23:59:31 +0330

    add orders screnn

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: ((context, index) => OrderItem(
+              orderData.orders[index],
+            )),
+      ),
+    );
+  }
+}
