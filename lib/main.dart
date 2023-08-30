c2a1fafd05c24a8e9994751ac3c9338aa370754d
khakestari <mostafanmz@yahoo.com>
2023-08-30 03:39:30 +0330

    Fading Loaded Images In & Showing a Placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/splash_screen.dart';
+import './providers/orders.dart';
 import './providers/auth.dart';
+import './providers/products.dart';
+import './providers/cart.dart';
+
+import './screens/splash_screen.dart';
 import './screens/auth-screen.dart';
 import './screens/edit_product_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/orders_screen.dart';
-import './providers/orders.dart';
 import './screens/cart_screen.dart';
-import './providers/cart.dart';
-// import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
-import './providers/products.dart';
 import './screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
